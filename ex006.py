5d4abb89555eac3cfa4598d52adf6adbbb154aed
Kleber Vieira <92216659+klebervieirati@users.noreply.github.com>
2021-10-15 14:37:28 -0300

    PY

--- ex006.py
+++ ex006.py
@@ -2,6 +2,6 @@ n = int(input('Digite um Número..: '))
 
 dobro =n*2
 triplo =n*3
-raiz =n**2
+raiz =n**(1/2)
 
 print('O DOBRO É {:=^20}\nO TRIPLO É {:=^20}\nA RAIZ QUANDRADA É {:=^20}'.format(dobro,triplo,raiz))
