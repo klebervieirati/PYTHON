962dd946da32080d86b177ec0c8c1f6954fd46b2
Kleber Vieira <92216659+klebervieirati@users.noreply.github.com>
2021-10-15 15:30:31 -0300

    Update ex007.py

--- ex007.py
+++ ex007.py
@@ -1,5 +1,5 @@
-nota1 = int(input('DIGITE A PRIMEIRA NOTA.: '))
-nota2 = int(input('DIGITE A SEGUNDA NOTA.: '))
-
+nota1 = float(input('\nDIGITE A PRIMEIRA NOTA.: '))
+nota2 = float(input('\nDIGITE A SEGUNDA NOTA.: '))
 media = (nota1+nota2)/2
-print('SUA MÉDIA É {}'.format(media))
+
+print('A Média entre {:.1f} e {:.1f} é igual a {:.1f} '.format(nota1,nota2,media))
